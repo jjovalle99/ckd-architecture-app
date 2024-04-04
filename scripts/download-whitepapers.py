3af5f9b166c32cedf76a2fccaaa3c196f74b71fd
jjovalle99 <jj.ovalle@uniandes.edu.co>
2024-04-03 22:39:33 -0500

    Naive Extraction

--- scripts/download-whitepapers.py
+++ scripts/download-whitepapers.py
@@ -11,19 +11,21 @@ def download_pdfs():
         whitepapers_data = json.load(f)
 
     os.makedirs(DOWNLOAD_DIRECTORY, exist_ok=True)
-
-    counter = 1
-    for whitepaper in whitepapers_data:
-        pdf_link = whitepaper["pdf_link"]
-        if pdf_link != "No PDF Link":
-            file_name = f"{counter}.pdf"
-            file_path = os.path.join(DOWNLOAD_DIRECTORY, file_name)
-            try:
-                subprocess.run(["wget", "-O", file_path, pdf_link], check=True)
-                print(f"Downloaded: {file_name}")
-                counter += 1
-            except subprocess.CalledProcessError:
-                print(f"Failed to download: {pdf_link}")
+    for year in [2020, 2021, 2022, 2023, 2024]:
+        os.makedirs(os.path.join(DOWNLOAD_DIRECTORY, str(year)), exist_ok=True)
+        subset = [wp for wp in whitepapers_data if wp["year"] == year]
+        counter = 1
+        for whitepaper in subset:
+            pdf_link = whitepaper["pdf_link"]
+            if pdf_link != "No PDF Link":
+                file_name = f"{year}/{counter}.pdf"
+                file_path = os.path.join(DOWNLOAD_DIRECTORY, file_name)
+                try:
+                    subprocess.run(["wget", "-O", file_path, pdf_link], check=True)
+                    print(f"Downloaded: {file_name}")
+                    counter += 1
+                except subprocess.CalledProcessError:
+                    print(f"Failed to download: {pdf_link}")
 
 
 if __name__ == "__main__":
